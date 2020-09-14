72bf3f6d3e687c1358b67fa9f2dfc6f8f2624a28
Vadiknevadik <themeneme@gmail.com>
2020-09-14 11:04:40 +0500

    avg_mark

--- marks.py
+++ marks.py
@@ -8,3 +8,11 @@ while True:
 
 print('Ввод завершен')
 print(student_marks)
+
+i = 0
+avg_mark = 0
+while i < len(student_marks):
+    avg_mark += int(student_marks[i])
+    i += 1
+avg_mark /= len(student_marks)
+print('Средний балл:',avg_mark)
